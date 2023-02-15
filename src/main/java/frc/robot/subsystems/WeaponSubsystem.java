d0860713e507b76fba323d04144ad67fee68ba6f
Zebrotics Programming <lincoln@vulpes.com>
2023-02-15 18:54:16 -0500

    added new controller for weapon

--- src/main/java/frc/robot/subsystems/WeaponSubsystem.java
+++ src/main/java/frc/robot/subsystems/WeaponSubsystem.java
@@ -10,6 +10,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class WeaponSubsystem extends SubsystemBase {
     public DigitalInput sensor1;
+
+
+    
     public WeaponSubsystem(){
         sensor1 = new DigitalInput(0);
     }
